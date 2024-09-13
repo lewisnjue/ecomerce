fc13f12b62b19d8545ab9717e6d40e29d14295ff
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-09-13 10:02:07 +0300

    changed databae configuration for testing

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -87,9 +87,10 @@ WSGI_APPLICATION = 'ecomerce.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-            default=env('DATABASE_URL')
-        )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
