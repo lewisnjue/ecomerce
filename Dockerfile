dc918706196761a86b684de1e5da2324ae73a451
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-09-13 11:33:04 +0300

    added docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.9.20-slim
+
+COPY . /ecomerce 
+
+WORKDIR /ecomerce
+
+RUN apt update
+
+RUN apt upgrade -y
+
+RUN python3 -m venv /opt/venv
+
+RUN /opt/venv/bin/pip install -r requirements.txt
+
+CMD /opt/venv/bin/gunicorn ecomerce.wsgi:application --bind 0.0.0.0:8000
+
+
--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -87,10 +87,9 @@ WSGI_APPLICATION = 'ecomerce.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+            default=env('DATABASE_URL')
+        )
 }
 
 
